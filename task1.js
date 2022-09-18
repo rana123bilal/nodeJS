42ad8e4bc84ba97c46b6b3bcc7c3123d6dd38ace
rana123bilal <rana123bilal@gmail.com>
2022-09-19 00:54:19 +0100

    updated task2

--- task1.js
+++ task1.js
@@ -1,5 +1,10 @@
-function reverseString(str){
-    const updatedStr = str.split('').reverse().join('')
-    return updatedStr;
-}
-console.log(reverseString('test data'))
+import readline from 'readline'
+
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+ 
+rl.on('line', (line) => {
+    const reversedInput = line.split('').reverse().join('')
+    console.log(reversedInput);
+});
--- task2.js
+++ task2.js
@@ -1,7 +1,29 @@
-const csvFilePath='./data.csv'
-import csv from 'csvtojson'; // ES6 import
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-	console.log(jsonObj); 
-})
+const csvFilePath='./files/data.csv';
+import fs from "fs";
+import {parse} from "csv-parse";
+
+let str = ''
+ 
+fs.createReadStream(csvFilePath)
+  .pipe(parse({ delimiter: ",", from_line: 2}))
+  .on("data", function (row) {
+      const obj = {
+          "book": row[0],
+          "author": row[1],
+          "price": row[2]
+      }
+      str += `${JSON.stringify(obj)}\n`
+  }) 
+  .on("end", function () {
+    console.log("done");
+    fs.writeFile('./files/text.txt', str, error => {
+        if (error) {
+            console.error(error);
+        }
+    });
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+
+ 
